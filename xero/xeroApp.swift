4e6bc323d7d5b054abfbbe29d96fd87bec859c14
Krutik Pandya <kpandya8@asu.edu>
2024-02-15 19:23:00 -0700

    Initial Commit

---
+++ xero/xeroApp.swift
@@ -0,0 +1,21 @@
+//
+//  xeroApp.swift
+//  xero
+//
+//  Created by Vrushik Mehta on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct xeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
